ff716baeabb5e98a437de694cf354926d6d09dd5
Doug Devitre <doug@dougdevitre.com>
2024-07-10 01:30:55 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function callDoug() {
+    window.location.href = 'tel:+13144965973';
+}
+
+function emailDoug() {
+    window.location.href = 'mailto:dougdevitre@gmail.com';
+}
+
+function visitLinkedIn() {
+    window.location.href = 'https://linkedin.com/in/dougdevitre';
+}
